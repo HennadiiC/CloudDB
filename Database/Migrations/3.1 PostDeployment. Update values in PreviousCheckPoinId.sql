dbf83769b9e30654e0165b7995ea50bd35c9236e
HennadiiC <hennadii.chmuzh@gmail.com>
2020-10-21 12:21:23 +0300

    Migration 3.1 Add new link column, which will replace old with numveration and adding post deployment script, that will set values to new column based on numeration column

---
+++ Database/Migrations/3.1 PostDeployment. Update values in PreviousCheckPoinId.sql
@@ -0,0 +1,13 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+update Checkpoints
+  set PrevCheckPointId = (select id from Checkpoints cp2 where cp2.PointNum = PointNum - 1 and RaceId = cp2.RaceId)
--- Database/dbo/Tables/Checkpoints.sql
+++ Database/dbo/Tables/Checkpoints.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TABLE [dbo].[Checkpoints] (
-    [Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [Name]     NVARCHAR (50) NULL,
-    [Distance] FLOAT (53)    NOT NULL,
-    [RaceId]   INT           NOT NULL,
-    [PointNum] INT           CONSTRAINT [DF_Checkpoints_PointNum] DEFAULT ((1)) NOT NULL,
+    [Id]               INT           IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (50) NULL,
+    [Distance]         FLOAT (53)    NOT NULL,
+    [RaceId]           INT           NOT NULL,
+    [PointNum]         INT           CONSTRAINT [DF_Checkpoints_PointNum] DEFAULT ((1)) NOT NULL,
+    [PrevCheckPointId] INT           NULL,
     CONSTRAINT [PK_Checkpoints] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Checkpoints_Checkpoints] FOREIGN KEY ([Id]) REFERENCES [dbo].[Checkpoints] ([Id]),
     CONSTRAINT [FK_Checkpoints_Racings] FOREIGN KEY ([RaceId]) REFERENCES [dbo].[Racings] ([Id])
 );
+
+
 
 
 
