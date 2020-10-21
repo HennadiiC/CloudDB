8a3ec29f3819dcc0ce14d42cc5c66a416ec9b184
HennadiiC <hennadii.chmuzh@gmail.com>
2020-10-21 13:18:44 +0300

    Migration 5 Split fulname onto two columns

--- Database/dbo/Tables/Racers.sql
+++ Database/dbo/Tables/Racers.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Racers] (
-    [Id]       INT          IDENTITY (1, 1) NOT NULL,
-    [FullName] VARCHAR (60) NOT NULL,
+    [Id]        INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (60) NOT NULL,
+    [LastName]  VARCHAR (60) CONSTRAINT [DF_Racers_LastName] DEFAULT ('Racer') NOT NULL,
     CONSTRAINT [PK_Racers] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
