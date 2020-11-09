83243e7fbf93c06e1c037dfd3ba844054b9baddc
HennadiiC <hennadii.chmuzh@gmail.com>
2020-11-09 21:55:12 +0200

    Schema improving

--- Database/dbo/Tables/Racers.sql
+++ Database/dbo/Tables/Racers.sql
@@ -2,8 +2,12 @@
     [Id]        INT          IDENTITY (1, 1) NOT NULL,
     [FirstName] VARCHAR (60) NOT NULL,
     [LastName]  VARCHAR (60) CONSTRAINT [DF_Racers_LastName] DEFAULT ('Racer') NOT NULL,
-    CONSTRAINT [PK_Racers] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [CountryId] INT          NOT NULL,
+    CONSTRAINT [PK_Racers] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Racers_Countries] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[Countries] ([Id])
 );
+
+
 
 
 
--- Database/dbo/Tables/Racings.sql
+++ Database/dbo/Tables/Racings.sql
@@ -2,6 +2,10 @@
     [Id]          INT            IDENTITY (1, 1) NOT NULL,
     [Name]        NVARCHAR (50)  NOT NULL,
     [Description] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Racings] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [SeasonId]    INT            NOT NULL,
+    CONSTRAINT [PK_Racings] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Racings_Seasons] FOREIGN KEY ([SeasonId]) REFERENCES [dbo].[Seasons] ([Id])
 );
+
+
 
