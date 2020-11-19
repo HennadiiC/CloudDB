75279e35e7a4f5f48c097a9c3a9e245f3ec55b37
HennadiiC <hennadii.chmuzh@gmail.com>
2020-11-19 21:59:35 +0200

    SignalR was added to startup

---
+++ RacingSite/Hubs/RaceHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RacingSite.Hubs
+{
+    public class RaceHub : Hub
+    {
+        public Task NotifyAboutCheckpointPassing()
+        {
+            return Clients.All.SendAsync("OnCheckpointPassed", new { p1 = 17, p2 = "XYZ" });
+        }
+    }
+}
--- RacingSite/Startup.cs
+++ RacingSite/Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RacingSite.Hubs;
 
 namespace RacingSite
 {
@@ -20,13 +16,13 @@ namespace RacingSite
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -39,6 +35,7 @@ namespace RacingSite
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -51,6 +48,7 @@ namespace RacingSite
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<RaceHub>("raceHub");
             });
         }
     }
