2d9e64b286285cf46c87dbc44e13a61624d9fddb
HennadiiC <hennadii.chmuzh@gmail.com>
2020-11-22 15:17:55 +0200

    Race emulator

--- RaceFiller/Program.cs
+++ RaceFiller/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Core;
 using Newtonsoft.Json;
 
@@ -8,8 +11,7 @@ namespace RaceFiller
 {
     class Program
     {
-        private static readonly string BaseUrl = "https://localhost:44379/";
-        //private static readonly string BaseUrl = "https://racing.azurewebsites.net/";
+        private static readonly string BaseUrl = 0 == 1 ? "https://localhost:44379/" : "https://racing.azurewebsites.net/";
 
         private static Random Rnd { get; } = new Random(2020);
 
@@ -17,41 +19,59 @@ namespace RaceFiller
 
         static void Main(string[] args)
         {
+            Start(6, 5, 2, 3);
+        }
+
+        private static void Start(int checkpointsCount, int racersCount, int perIntervalCount, int intervalInSeconds)
+        {
             var race = new Race
             {
                 Id = 1,
-                Name = "Race 1 from console 2",
-                Checkpoints = new[]
-                {
-                    new Checkpoint
-                    {
-                        Id = 2,
-                        Name = "Checkpoint 2"
-                    },
-                    new Checkpoint
-                    {
-                        Id = 3,
-                        Name = "Checkp 3"
-                    },
-                }
+                Name = $"Race {DateTime.Now:T}",
+                Checkpoints = Enumerable.Range(1, checkpointsCount)
+                    .Select(i => new Checkpoint { Id = i, Name = $"Checkpoint #{i}" })
+                    .ToArray()
             };
-            var racers = new[]
-            {
-                new Racer { Id = 4, Name = "Racer 4" },
-                new Racer { Id = 5, Name = "Racer 5" },
-            };
+
+            var racers = Enumerable.Range(1, racersCount)
+                .Select(i => new Racer { Id = i, Name = $"Racer {i}" }).ToArray();
 
             Initialize(race, racers);
 
-            var cpp = new CheckpointPassing
-            {
-                Id = 6,
-                RacerId = racers[0].Id,
-                CheckpointId = race.Checkpoints[0].Id,
-                Time = DateTime.Now,
-            };
+            Console.ReadKey();
 
-            AddCheckpointPassing(cpp);
+            var groups = Enumerable.Range(1, racersCount)
+                .GroupBy(i => (i - 1) / perIntervalCount)
+                .Select(g => g.ToArray())
+                .ToArray();
+
+            var tasks = new List<Task>();
+
+            foreach (var racersInGroup in groups)
+            {
+                foreach (var id in racersInGroup)
+                {
+                    var racerId = id;
+                    var t = Task.Run(() =>
+                    {
+                        foreach (var checkpoint in race.Checkpoints)
+                        {
+                            AddCheckpointPassing(new CheckpointPassing
+                            {
+                                CheckpointId = checkpoint.Id,
+                                RacerId = racerId,
+                                Id = 0,
+                                Time = DateTime.Now,
+                            });
+                            Thread.Sleep(Rnd.Next(0, 10) * 1000);
+                        }
+                    });
+                    tasks.Add(t);
+                }
+                Thread.Sleep(intervalInSeconds * 1000);
+            }
+
+            Task.WaitAll(tasks.ToArray());
         }
 
         private static void Initialize(Race race, Racer[] racers)
@@ -95,7 +115,7 @@ namespace RaceFiller
                 .AsParallel()
                 .Select(rn =>
                 {
-                    
+
                     return $"Race {rn} in season";
                 })
                 .ToArray();
